a6e7458cb001434001a6001b5b25f5b014415f2e
SZF1 A2 <szf1_a2@oktatas.local>
2022-05-20 12:10:33 +0200

    Torpedo teszt osztály

---
+++ src/main/TorpedoProgram.java
@@ -0,0 +1,11 @@
+
+package main;
+
+public class TorpedoProgram {
+
+ 
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/main/TorpedoTeszt.java
@@ -0,0 +1,13 @@
+
+package main;
+
+
+public class TorpedoTeszt {
+    public static void main(String[] args) {
+        
+    }
+    
+    public String tesztLoves (int poz){
+        return "";
+    }
+}
