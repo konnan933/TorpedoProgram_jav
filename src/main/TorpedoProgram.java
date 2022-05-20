d040eb0ab8c139b04003cd7bfdb162d6aca68f93
konnan933 <konnan933@gmail.com>
2022-05-20 13:26:25 +0200

    TorpedoProram-ban elhelyezve a mainben

--- src/main/TorpedoProgram.java
+++ src/main/TorpedoProgram.java
@@ -5,7 +5,8 @@ public class TorpedoProgram {
 
  
     public static void main(String[] args) {
-        
+        TorpedoTeszt jatek = new TorpedoTeszt();
+        jatek.tesztLoves(4);
     }
     
 }
