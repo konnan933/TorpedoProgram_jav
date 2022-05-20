b65368968a460c947d15cc11b05faa74775f391e
konnan933 <konnan933@gmail.com>
2022-05-20 12:21:02 +0200

    hajo.talalat() assert ellenörzés torpedoTesztben

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -4,10 +4,14 @@ package main;
 
 public class TorpedoTeszt {
     public static void main(String[] args) {
-        
+        tesztLoves(4);
     }
     
-    public String tesztLoves (int poz){
-        return "";
+    public static String tesztLoves (int poz){
+        Hajo hajo = new Hajo();
+        String t= hajo.talalat(poz);
+        assert t.equals("talált"):"nem jó a találat ellenörzés";
+                
+        return"";
     }
 }
